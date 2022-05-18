f3fc8b93e7c101d36dd0d2f671b8833b9f0fe2b5
Matthias Schneller <matthias.schneller@gmail.com>
2022-05-18 12:11:16 +0200

    remove the renormalization

--- src/components.jl
+++ src/components.jl
@@ -27,7 +27,6 @@ are in the varargs `comps`.
 See also: [`propagation!`](@ref)
 """
 function Setup(comps::Component...)::Setup
-    # comps...  varargs for the input of the components
     return Setup(collect(comps))
 end
 
@@ -42,32 +41,8 @@ The wave will be altered according to the propagation.
 See also: [`Setup`](@ref)
 """
 function propagation!(wave::Wave, setup::Setup)
-    # wave    ...   The wave that should be propagated
-    # setup   ...   The setup through which the propagation should take place
-
-    # iterate through the setup
     for comp in setup.setup
         calculate!(wave, comp)
     end
-
-    # normalization
-    _normalization!(wave)
 end
 
-"""
-    _normalization!(wave::Wave)
-
-Normalize the wave.
-
-This function will normalize the wave that is given in `wave`.
-For that it calculates the absoltue square and matches it to the normalizaion
-paramter `wave.norm` that is given in the wave type.
-"""
-function _normalization!(wave::Wave)
-    # wave   ...   either an electron or light wave
- 
-    # Normalize the wave function
-    wave.ψ .*= sqrt(wave.norm / sum(abs2.(wave.ψ)) /
-                    abs(wave.x[1] - wave.x[2]) /
-                    abs(wave.y[1] - wave.y[2]))
-end
--- src/electronBeam.jl
+++ src/electronBeam.jl
@@ -1,65 +1,31 @@
-
-
 """
-    Create the electron beam type.
-"""
-mutable struct ElectronBeam <: Wave
-    ψ::Array{<:Complex}     # The wave function
-    λ::Real                 # de'Broglie wavelength
-    x::Array{<:Real}        # x-axis
-    y::Array{<:Real}        # y-axis
-    norm::Real              # normalization parameter
-end
-
-
-"""
-ElectronBeam(ψ::Matrix{<:Complex}, x::Vector{<:AbstractFloat},
-             y::Vector{<:AbstractFloat}, U::Real)::ElectronBeam
+    ElectronBeam(ψ::Matrix{<:Complex}, λ::Real, x::Vector{<:Real}, y::Vector{<:Real})
 
 Return the ElectronBeam type.
 
-The electron beam type will be created and returned with this function.
-The `x` value is the x-axis of the geometry, the `y` value is the y-axis
-of the geometry, and `U` is the acceleration voltage of the Electrons.
+In the ElectronBeam type the wave function `ψ`, the de Broglie wavelength `λ` as well as
+the coordinates `x` and `y` are saved.
 
-The input wave `ψ` will be normalized and saved.
 
 # Example
 ```jldoctest
-julia> x = Array{Float64}(1:2);
+julia> ψ = ones(ComplexF64, 2, 2)
+2×2 Matrix{ComplexF64}:
+ 1.0+0.0im  1.0+0.0im
+ 1.0+0.0im  1.0+0.0im
 
-julia> y = Array{Float64}(1:2);
-
-julia> ψ = ones(ComplexF64, 2 , 2);
-
-julia> U = 1;
-
-julia> ElectronBeam(ψ, x, y, U)
-ElectronBeam(ComplexF64[0.25 + 0.0im 0.25 + 0.0im; 0.25 + 0.0im 0.25 + 0.0im], 1.2264259654066947e-9, [1.0, 2.0], [1.0, 2.0], 2, 2, 1.0)
-
+julia> ElectronBeam(ψ, 1e-12, [1, 2], [1, 2])
+ElectronBeam(ComplexF64[1.0 + 0.0im 1.0 + 0.0im; 1.0 + 0.0im 1.0 + 0.0im], 1.0e-12, [1, 2], [1, 2])
 
 ```
-
-See also: [`LaserBeam`](@ref)
 """
-function ElectronBeam(ψ::Matrix{<:Complex}, x::Vector{<:AbstractFloat},
-                      y::Vector{<:AbstractFloat}, U::Real)::ElectronBeam
-    # x   ...   the x axis of the geometry
-    # y   ...   the y axis of the geometry
-    # U   ...   the acceleration voltage of the electron beam
-
-    # define the de'Broglie wavelength of the electrons
-    λ = 2 * π * ħ / sqrt( 2 * U * q * m_e) / sqrt(1 + q * U / 2 / m_e / c^2)
-
-    # normalize the input beam
-    ψ ./= sqrt(sum(abs2.(ψ))*abs(x[1]-x[2])*abs(y[1]-y[2]))
-
-    # set the normalization parameter to 1 (100% of the electrons are present)
-    norm = 1.
-
-    # return the electron beam struct
-    return ElectronBeam(ψ, λ, x, y, norm)
+mutable struct ElectronBeam <: Wave
+    ψ::Matrix{<:Complex}
+    λ::Real
+    x::Vector{<:Real}
+    y::Vector{<:Real}
 end
+
 
 """
     deBroglieWavelength(U::Real)
